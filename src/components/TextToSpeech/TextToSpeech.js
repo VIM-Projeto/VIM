3164e88c11bb03faa53e92ed3e175dd75d2e8ee9
VisuallyImpairedMap <visuallyimpairedmap@gmail.com>
2024-10-05 14:14:50 -0300

    second commit

--- src/components/TextToSpeech/TextToSpeech.js
+++ src/components/TextToSpeech/TextToSpeech.js
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 function TextToSpeech() {
     useEffect(() => {
         const text = 'O projeto Visually Impaired Map é uma iniciativa dedicada a auxiliar pessoas com deficiência visual a chegarem a locais específicos. Por meio de um aplicativo para smartphones, os usuários podem indicar o destino desejado por comando de voz, e o app traçará a rota até lá. Utilizando o alto-falante do dispositivo, o aplicativo fornecerá instruções verbais para o usuário indicando quando seguir em frente, virar à esquerda, virar à direita ou voltar, conforme necessário.';
 
-        if (!window.speechSynthesis.speaking) {
-            const utterance = new SpeechSynthesisUtterance(text);
-            window.speechSynthesis.speak(utterance);
-        }
+       
+        const timeoutId = setTimeout(() => {
+            if (!window.speechSynthesis.speaking) {
+                const utterance = new SpeechSynthesisUtterance(text);
+                window.speechSynthesis.speak(utterance);
+            }
+        }, 1000); 
+
+        return () => clearTimeout(timeoutId);
     }, []); 
 
     return null; 
 }
 
-export default TextToSpeech;
+export default TextToSpeech; 
+
